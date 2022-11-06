3b6f1ca5f529500d51bb87ed2c4e9e5d32bac7c7
Arkadiusz Kniaź <56119871+arekkn@users.noreply.github.com>
2022-11-06 21:40:23 +0100

    Update embedding101.py

--- steps/embedding101.py
+++ steps/embedding101.py
@@ -10,7 +10,5 @@ resnet101_model = torchvision.models.resnet101(weights=resnet101_weights)
 def save_resnet101_embeddings(img_name_list: list) -> list:
     img_embeddings_list = []
     for path in img_name_list:
-        if random.random()<0.05:
-            print('dupa')
         img_embeddings_list.append(eval(Image.open(path), resnet101_model, resnet101_weights))
     return img_embeddings_list

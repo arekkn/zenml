0531186619fa87470c87889d197d642e2f832aca
arek <jak47l.arekkn@gmail.com>
2022-11-06 17:03:36 +0100

    commit file

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,51 @@ import is_cat
 from PIL import Image
 import requests
 from io import BytesIO
+####dodaje
+from utils import eval
+from utils import eval_vit
+from transformers import ViTFeatureExtractor
+from transformers import ViTForImageClassification
+import torchvision.models
 
 @st.cache
 def load_image(img_file):
     img=Image.open(img_file)
     return img
+ 
+###dodaje   
 @st.cache
-def find_similar(img_file):
-    files=[os.path.join("Korat", f) for f in os.listdir("Korat")]
-    return files
+def find_closest_img(img):
+    df = pd.read_pickle("./embeddings.pkl", compression="xz") 
+    our_cat = df.loc[df["img_paths"]==img]
+    result_img_list = []
+    for col in df.columns[1:]:
+        d = np.stack((df[col].to_numpy() - our_cat[col].to_numpy())**2).sum(axis=1)
+        d[d==0] = 1000
+        idx = d.argmin()
+        result_img_list.append(df.iloc[idx]["img_paths"])
+    return result_img_list
+
+@st.cache
+def save_new_embedings(img_path):
+    embeddings_df = pd.read_pickle("embeddings.pkl")
+    resnet18_weights = torchvision.models.ResNet18_Weights.DEFAULT
+    resnet18_model = torchvision.models.resnet18(weights=resnet18_weights)
+    resnet34_weights = torchvision.models.ResNet34_Weights.DEFAULT
+    resnet34_model = torchvision.models.resnet34(weights=resnet34_weights)
+    resnet50_weights = torchvision.models.ResNet50_Weights.DEFAULT
+    resnet50_model = torchvision.models.resnet50(weights=resnet50_weights)
+    feature_extractor = ViTFeatureExtractor('google/vit-base-patch16-224')
+    embedding_model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+    result = pd.DataFrame({
+        "img_paths":[img_path],
+        "resnet18_embeddings":[eval(Image.open(img_path), resnet18_model, resnet18_weights)],
+        "resnet34_embeddings":[eval(Image.open(img_path), resnet34_model, resnet34_weights)],
+        "resnet50_embeddings":[eval(Image.open(img_path), resnet50_model, resnet50_weights)],
+        "vit_embeddings": [eval_vit(Image.open(img_path), embedding_model, feature_extractor)]})
+    output = pd.concat([embeddings_df, result])
+    output.to_pickle("./embeddings.pkl")
+
 
 @st.cache
 def scale_photo(img):
@@ -31,10 +67,14 @@ def main():
         st.text("So this is your picture")
         st.image(load_image(img_file))
         st.write(is_cat.is_cat(load_image(img_file)))
+        #####dodaje
+        img_path=os.path.join("cats/", img_file.name, "wb")
+        with open(os.path.join(img_path)) as f:
+            f.write(img_file.getbuffer())
+        save_new_embedings(img_path)
+        recomendation_list = find_closest_img(img_path)
+
         if not is_cat.is_cat(load_image(img_file)):
-            st.text("run start")
-            run_2()
-            st.text("run stop")
             st.text("And our model says he doesn't see a cat in this photo, instead he sees:")
             st.write(is_cat.what_is(load_image(img_file)))
         st.text("And our model ")
@@ -42,41 +82,30 @@ def main():
         buf = BytesIO()
         col1, col2,col3= st.columns(3)
         col1.write("This is cat 1")
-        img_cat1 = scale_photo(load_image(find_similar(img_file)[0]))
+        img_cat1 = scale_photo(load_image(find_closest_img(img_file)[0]))
         col1.image(img_cat1)
         img_cat1.save(buf, format="JPEG")
         byte_im = buf.getvalue()
         col1.download_button(label='Download cat 1',data=byte_im,file_name="cat1.jpg",mime="image/jpeg")
         col2.write("This is cat 2")
-        img_cat2 = scale_photo(load_image(find_similar(img_file)[1]))
+        img_cat2 = scale_photo(load_image(find_closest_img(img_file)[1]))
         col2.image(img_cat2)
         img_cat2.save(buf, format="JPEG")
         byte_im = buf.getvalue()
         col2.download_button(label='Download cat 2',data=byte_im,file_name="cat2.jpg",mime="image/jpeg")
         col3.write("This is cat 3")
-        img_cat3 = scale_photo(load_image(find_similar(img_file)[2]))
+        img_cat3 = scale_photo(load_image(find_closest_img(img_file)[2]))
         col3.image(img_cat3)
         img_cat3.save(buf, format="JPEG")
         byte_im = buf.getvalue()
         col3.download_button(label='Download cat 3',data=byte_im,file_name="cat3.jpg",mime="image/jpeg")
         col1.write("This is cat 4")
-        img_cat4 = scale_photo(load_image(find_similar(img_file)[3]))
+        img_cat4 = scale_photo(load_image(find_closest_img(img_file)[3]))
         col1.image(img_cat4)
         img_cat4.save(buf, format="JPEG")
         byte_im = buf.getvalue()
         col1.download_button(label='Download cat 4',data=byte_im,file_name="cat4.jpg",mime="image/jpeg")
-        col2.write("This is cat 5")
-        img_cat5 = scale_photo(load_image(find_similar(img_file)[4]))
-        col2.image(img_cat5)
-        img_cat5.save(buf, format="JPEG")
-        byte_im = buf.getvalue()
-        col2.download_button(label='Download cat 5',data=byte_im,file_name="cat5.jpg",mime="image/jpeg")
-        col3.write("This is cat 6")
-        img_cat6 = scale_photo(load_image(find_similar(img_file)[5]))
-        col3.image(img_cat6)
-        img_cat6.save(buf, format="JPEG")
-        byte_im = buf.getvalue()
-        col3.download_button(label='Download cat 6',data=byte_im,file_name="cat6.jpg",mime="image/jpeg")
+        
         st.text('Please send feedback')
         message = st.text_input('message')
         #for f in find_similar(img_file):

90196f385bca207ab223c8c2786ec388a6727638
Arkadiusz Kniaź <56119871+arekkn@users.noreply.github.com>
2022-11-06 21:41:13 +0100

    Update embedding18.py

--- steps/embedding18.py
+++ steps/embedding18.py
@@ -10,7 +10,5 @@ resnet18_model = torchvision.models.resnet18(weights=resnet18_weights)
 def save_resnet18_embeddings(img_name_list: list) -> list:
     img_embeddings_list = []
     for path in img_name_list:
-        if random.random()<0.05:
-            print('dupa')
         img_embeddings_list.append(eval(Image.open(path), resnet18_model, resnet18_weights))
     return img_embeddings_list
